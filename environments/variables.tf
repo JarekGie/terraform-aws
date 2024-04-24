8fe89fca4b26541833bb97c7155d539b18bedd8e
Jarek Golab <jaroslaw.golab@beaconcure.com>
2024-04-24 16:12:33 +0200

    added varialble.tf file

---
+++ environments/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket" {
+  description = "Bucket name"
+}
+variable "dynamodb_table_name" {
+  description = "Lock table name"
+}
+variable "region" {
+  description = "Default region for resources"
+}
